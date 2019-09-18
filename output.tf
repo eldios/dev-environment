035194b59b550a308ae9ac97cbdb3a75b80e472a
Emanuele 'Lele' Calo <lele@sshadm.in>
2019-09-18 18:36:26 +0200

    Improved TF output to spit out a perfect ssh_config output

--- output.tf
+++ output.tf
@@ -1,9 +1,45 @@
-output "DEV-VMs-IPs" {
-    value = "${join("\n",google_compute_instance.dev.*.network_interface.0.access_config.0.nat_ip)}"
+output "shared_ssh_config"{
+  value = <<-SSHCONFIG
+
+Host ${var.env_name}.* 
+  User ${var.user}
+  Port 22
+  UserKnownHostsFile /dev/null
+  StrictHostKeyChecking no
+  PasswordAuthentication no
+  ForwardAgent yes 
+  IdentitiesOnly yes 
+  LogLevel FATAL
+  IdentityFile ${var.ssh_keyfile}
+SSHCONFIG
 }
-output "kubeadm-VMs-IPs" {
-    value = "${join("\n",google_compute_instance.kubeadm.*.network_interface.0.access_config.0.nat_ip)}"
+
+output "dev_ssh_config"{
+    value = <<-SSHCONFIG
+%{ for host in google_compute_instance.dev.* ~}
+
+Host ${host.name}
+    Hostname ${host.network_interface.0.access_config.0.nat_ip}
+%{ endfor ~}
+SSHCONFIG
 }
-output "test-VMs-IPs" {
-    value = "${join("\n",google_compute_instance.test.*.network_interface.0.access_config.0.nat_ip)}"
+
+output "kubeadmVM_ssh_config"{
+    value = <<-SSHCONFIG
+%{ for host in google_compute_instance.kubeadm.* ~}
+
+Host ${host.name}
+    Hostname ${host.network_interface.0.access_config.0.nat_ip}
+%{ endfor ~}
+SSHCONFIG
+}
+
+output "test_ssh_config"{
+    value = <<-SSHCONFIG
+%{ for host in google_compute_instance.test.* ~}
+
+Host ${host.name}
+    Hostname ${host.network_interface.0.access_config.0.nat_ip}
+%{ endfor ~}
+SSHCONFIG
 }
